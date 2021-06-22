33cf0ecb11836df2bdce02609d65c56201d487fa
ChandlerFerry <chandlerferry@ferrysystems.io>
2021-06-21 22:31:38 -0700

    chore: prepare testnet config

--- deafwave/consensus/default_constants.py
+++ deafwave/consensus/default_constants.py
@@ -32,10 +32,10 @@ testnet_kwargs = {
     # Forks of deafwave should change this value to provide replay attack protection. This is set to mainnet genesis chall
     "AGG_SIG_ME_ADDITIONAL_DATA": bytes.fromhex("ccd5bb71183532bff220ba46c268991a3ff07eb358e8255a65c30a2dce0e5fbb"),
     "GENESIS_PRE_FARM_POOL_PUZZLE_HASH": bytes.fromhex(
-        "d23da14695a188ae5708dd152263c4db883eb27edeb936178d4d988b8f3ce5fc"
+        "030df0a076a3ee729e533b94ec2dfdf51a3dbf36bd2783c9736aa847570abb06"
     ),
     "GENESIS_PRE_FARM_FARMER_PUZZLE_HASH": bytes.fromhex(
-        "3d8765d3a597ec1d99663f6c9816d915b9f68613ac94009884c4addaefcce6af"
+        "030df0a076a3ee729e533b94ec2dfdf51a3dbf36bd2783c9736aa847570abb06"
     ),
     "MAX_VDF_WITNESS_SIZE": 64,
     # Size of mempool = 50x the size of block

Chia-Network/chia-blockchain

New RPC to check keys prior to deleting them (#6894)
* New RPC to check keys prior to deleting them

* Updates based on PR comments

* Make sure to stop wallet before start again

chia/rpc/wallet_rpc_api.py
@@ -25,7 +25,7 @@ from chia.util.ws_message import WsRpcMessage, create_payload_dict
 from chia.wallet.cc_wallet.cc_wallet import CCWallet
 from chia.wallet.derive_keys import master_sk_to_singleton_owner_sk
 from chia.wallet.rl_wallet.rl_wallet import RLWallet
-from chia.wallet.derive_keys import master_sk_to_farmer_sk, master_sk_to_pool_sk
+from chia.wallet.derive_keys import master_sk_to_farmer_sk, master_sk_to_pool_sk, master_sk_to_wallet_sk
 from chia.wallet.did_wallet.did_wallet import DIDWallet
 from chia.wallet.trade_record import TradeRecord
 from chia.wallet.transaction_record import TransactionRecord
@@ -35,6 +35,8 @@ from chia.wallet.util.transaction_type import TransactionType
 from chia.wallet.util.wallet_types import WalletType
 from chia.wallet.wallet_info import WalletInfo
 from chia.wallet.wallet_node import WalletNode
+from chia.util.config import load_config
+from chia.consensus.coinbase import create_puzzlehash_for_pk
 
 # Timeout for response from wallet/full node for sending a transaction
 TIMEOUT = 30
@@ -57,6 +59,7 @@ class WalletRpcApi:
             "/generate_mnemonic": self.generate_mnemonic,
             "/add_key": self.add_key,
             "/delete_key": self.delete_key,
+            "/check_delete_key": self.check_delete_key,
             "/delete_all_keys": self.delete_all_keys,
             # Wallet node
             "/get_sync_status": self.get_sync_status,
@@ -269,6 +272,75 @@ class WalletRpcApi:
         if path.exists():
             path.unlink()
         return {}
+
+    async def _check_key_used_for_rewards(
+        self, new_root: Path, sk: PrivateKey, max_ph_to_search: int
+    ) -> Tuple[bool, bool]:
+        """Checks if the given key is used for either the farmer rewards or pool rewards
+        returns a tuple of two booleans
+        The first is true if the key is used as the Farmer rewards, otherwise false
+        The second is true if the key is used as the Pool rewards, otherwise false
+        Returns both false if the key cannot be found with the given fingerprint
+        """
+        if sk is None:
+            return False, False
+
+        config: Dict = load_config(new_root, "config.yaml")
+        farmer_target = config["farmer"].get("xch_target_address")
+        pool_target = config["pool"].get("xch_target_address")
+        found_farmer = False
+        found_pool = False
+        selected = config["selected_network"]
+        prefix = config["network_overrides"]["config"][selected]["address_prefix"]
+        for i in range(max_ph_to_search):
+            if found_farmer and found_pool:
+                break
+
+            ph = encode_puzzle_hash(create_puzzlehash_for_pk(master_sk_to_wallet_sk(sk, uint32(i)).get_g1()), prefix)
+
+            if ph == farmer_target:
+                found_farmer = True
+            if ph == pool_target:
+                found_pool = True
+
+        return found_farmer, found_pool
+
+    async def check_delete_key(self, request):
+        """Check the key use prior to possible deletion
+        checks whether key is used for either farm or pool rewards
+        checks if any wallets have a non-zero balance
+        """
+        used_for_farmer: bool = False
+        used_for_pool: bool = False
+        walletBalance: bool = False
+
+        fingerprint = request["fingerprint"]
+        sk, _ = await self._get_private_key(fingerprint)
+        if sk is not None:
+            used_for_farmer, used_for_pool = await self._check_key_used_for_rewards(self.service.root_path, sk, 100)
+
+            if self.service.logged_in_fingerprint != fingerprint:
+                await self._stop_wallet()
+                await self.service._start(fingerprint=fingerprint, skip_backup_import=True)
+
+            async with self.service.wallet_state_manager.lock:
+                wallets: List[WalletInfo] = await self.service.wallet_state_manager.get_all_wallet_info_entries()
+                for w in wallets:
+                    wallet = self.service.wallet_state_manager.wallets[w.id]
+                    unspent = await self.service.wallet_state_manager.coin_store.get_unspent_coins_for_wallet(w.id)
+                    balance = await wallet.get_confirmed_balance(unspent)
+                    pending_balance = await wallet.get_unconfirmed_balance(unspent)
+
+                    if (balance + pending_balance) > 0:
+                        walletBalance = True
+                        break
+
+        return {
+            "fingerprint": fingerprint,
+            "used_for_farmer_rewards": used_for_farmer,
+            "used_for_pool_rewards": used_for_pool,
+            "wallet_balance": walletBalance,
+        }
 
     async def delete_all_keys(self, request: Dict):
         await self._stop_wallet()

tests/wallet/rpc/test_wallet_rpc.py
@@ -1,4 +1,5 @@
 import asyncio
+from chia.util.config import load_config, save_config
 import logging
 from pathlib import Path
 
@@ -13,6 +14,8 @@ from chia.rpc.wallet_rpc_client import WalletRpcClient
 from chia.simulator.simulator_protocol import FarmNewBlockProtocol
 from chia.types.peer_info import PeerInfo
 from chia.util.bech32m import encode_puzzle_hash
+from chia.consensus.coinbase import create_puzzlehash_for_pk
+from chia.wallet.derive_keys import master_sk_to_wallet_sk
 from chia.util.ints import uint16, uint32
 from chia.wallet.transaction_record import TransactionRecord
 from tests.setup_nodes import bt, setup_simulators_and_wallets, self_hostname
@@ -241,6 +244,36 @@ class TestWalletRpc:
             await client.log_in_and_skip(pks[1])
             sk_dict = await client.get_private_key(pks[1])
             assert sk_dict["fingerprint"] == pks[1]
+
+            # Add in reward addresses into farmer and pool for testing delete key checks
+            # set farmer to first private key
+            sk = wallet_node.get_key_for_fingerprint(pks[0])
+            test_ph = create_puzzlehash_for_pk(master_sk_to_wallet_sk(sk, uint32(0)).get_g1())
+            test_config = load_config(wallet_node.root_path, "config.yaml")
+            test_config["farmer"]["xch_target_address"] = encode_puzzle_hash(test_ph, "txch")
+            # set pool to second private key
+            sk = wallet_node.get_key_for_fingerprint(pks[1])
+            test_ph = create_puzzlehash_for_pk(master_sk_to_wallet_sk(sk, uint32(0)).get_g1())
+            test_config["pool"]["xch_target_address"] = encode_puzzle_hash(test_ph, "txch")
+            save_config(wallet_node.root_path, "config.yaml", test_config)
+
+            # Check first key
+            sk_dict = await client.check_delete_key(pks[0])
+            assert sk_dict["fingerprint"] == pks[0]
+            assert sk_dict["used_for_farmer_rewards"] is True
+            assert sk_dict["used_for_pool_rewards"] is False
+
+            # Check second key
+            sk_dict = await client.check_delete_key(pks[1])
+            assert sk_dict["fingerprint"] == pks[1]
+            assert sk_dict["used_for_farmer_rewards"] is False
+            assert sk_dict["used_for_pool_rewards"] is True
+
+            # Check unknown key
+            sk_dict = await client.check_delete_key(123456)
+            assert sk_dict["fingerprint"] == 123456
+            assert sk_dict["used_for_farmer_rewards"] is False
+            assert sk_dict["used_for_pool_rewards"] is False
 
             await client.delete_key(pks[0])
             await client.log_in_and_skip(pks[1])

wolfrage76/caldera-blockchain

caldera/rpc/wallet_rpc_api.py
@@ -25,7 +25,7 @@ from caldera.util.ws_message import WsRpcMessage, create_payload_dict
 from caldera.wallet.cc_wallet.cc_wallet import CCWallet
 from caldera.wallet.derive_keys import master_sk_to_singleton_owner_sk
 from caldera.wallet.rl_wallet.rl_wallet import RLWallet
-from caldera.wallet.derive_keys import master_sk_to_farmer_sk, master_sk_to_pool_sk
+from caldera.wallet.derive_keys import master_sk_to_farmer_sk, master_sk_to_pool_sk, master_sk_to_wallet_sk
 from caldera.wallet.did_wallet.did_wallet import DIDWallet
 from caldera.wallet.trade_record import TradeRecord
 from caldera.wallet.transaction_record import TransactionRecord
@@ -35,6 +35,8 @@ from caldera.wallet.util.transaction_type import TransactionType
 from caldera.wallet.util.wallet_types import WalletType
 from caldera.wallet.wallet_info import WalletInfo
 from caldera.wallet.wallet_node import WalletNode
+from caldera.util.config import load_config
+from caldera.consensus.coinbase import create_puzzlehash_for_pk
 
 # Timeout for response from wallet/full node for sending a transaction
 TIMEOUT = 30
@@ -57,6 +59,7 @@ class WalletRpcApi:
             "/generate_mnemonic": self.generate_mnemonic,
             "/add_key": self.add_key,
             "/delete_key": self.delete_key,
+            "/check_delete_key": self.check_delete_key,
             "/delete_all_keys": self.delete_all_keys,
             # Wallet node
             "/get_sync_status": self.get_sync_status,
@@ -269,6 +272,75 @@ class WalletRpcApi:
         if path.exists():
             path.unlink()
         return {}
+
+    async def _check_key_used_for_rewards(
+        self, new_root: Path, sk: PrivateKey, max_ph_to_search: int
+    ) -> Tuple[bool, bool]:
+        """Checks if the given key is used for either the farmer rewards or pool rewards
+        returns a tuple of two booleans
+        The first is true if the key is used as the Farmer rewards, otherwise false
+        The second is true if the key is used as the Pool rewards, otherwise false
+        Returns both false if the key cannot be found with the given fingerprint
+        """
+        if sk is None:
+            return False, False
+
+        config: Dict = load_config(new_root, "config.yaml")
+        farmer_target = config["farmer"].get("xch_target_address")
+        pool_target = config["pool"].get("xch_target_address")
+        found_farmer = False
+        found_pool = False
+        selected = config["selected_network"]
+        prefix = config["network_overrides"]["config"][selected]["address_prefix"]
+        for i in range(max_ph_to_search):
+            if found_farmer and found_pool:
+                break
+
+            ph = encode_puzzle_hash(create_puzzlehash_for_pk(master_sk_to_wallet_sk(sk, uint32(i)).get_g1()), prefix)
+
+            if ph == farmer_target:
+                found_farmer = True
+            if ph == pool_target:
+                found_pool = True
+
+        return found_farmer, found_pool
+
+    async def check_delete_key(self, request):
+        """Check the key use prior to possible deletion
+        checks whether key is used for either farm or pool rewards
+        checks if any wallets have a non-zero balance
+        """
+        used_for_farmer: bool = False
+        used_for_pool: bool = False
+        walletBalance: bool = False
+
+        fingerprint = request["fingerprint"]
+        sk, _ = await self._get_private_key(fingerprint)
+        if sk is not None:
+            used_for_farmer, used_for_pool = await self._check_key_used_for_rewards(self.service.root_path, sk, 100)
+
+            if self.service.logged_in_fingerprint != fingerprint:
+                await self._stop_wallet()
+                await self.service._start(fingerprint=fingerprint, skip_backup_import=True)
+
+            async with self.service.wallet_state_manager.lock:
+                wallets: List[WalletInfo] = await self.service.wallet_state_manager.get_all_wallet_info_entries()
+                for w in wallets:
+                    wallet = self.service.wallet_state_manager.wallets[w.id]
+                    unspent = await self.service.wallet_state_manager.coin_store.get_unspent_coins_for_wallet(w.id)
+                    balance = await wallet.get_confirmed_balance(unspent)
+                    pending_balance = await wallet.get_unconfirmed_balance(unspent)
+
+                    if (balance + pending_balance) > 0:
+                        walletBalance = True
+                        break
+
+        return {
+            "fingerprint": fingerprint,
+            "used_for_farmer_rewards": used_for_farmer,
+            "used_for_pool_rewards": used_for_pool,
+            "wallet_balance": walletBalance,
+        }
 
     async def delete_all_keys(self, request: Dict):
         await self._stop_wallet()

tests/wallet/rpc/test_wallet_rpc.py
@@ -1,4 +1,5 @@
 import asyncio
+from caldera.util.config import load_config, save_config
 import logging
 from pathlib import Path
 
@@ -13,6 +14,8 @@ from caldera.rpc.wallet_rpc_client import WalletRpcClient
 from caldera.simulator.simulator_protocol import FarmNewBlockProtocol
 from caldera.types.peer_info import PeerInfo
 from caldera.util.bech32m import encode_puzzle_hash
+from caldera.consensus.coinbase import create_puzzlehash_for_pk
+from caldera.wallet.derive_keys import master_sk_to_wallet_sk
 from caldera.util.ints import uint16, uint32
 from caldera.wallet.transaction_record import TransactionRecord
 from tests.setup_nodes import bt, setup_simulators_and_wallets, self_hostname
@@ -241,6 +244,36 @@ class TestWalletRpc:
             await client.log_in_and_skip(pks[1])
             sk_dict = await client.get_private_key(pks[1])
             assert sk_dict["fingerprint"] == pks[1]
+
+            # Add in reward addresses into farmer and pool for testing delete key checks
+            # set farmer to first private key
+            sk = wallet_node.get_key_for_fingerprint(pks[0])
+            test_ph = create_puzzlehash_for_pk(master_sk_to_wallet_sk(sk, uint32(0)).get_g1())
+            test_config = load_config(wallet_node.root_path, "config.yaml")
+            test_config["farmer"]["xch_target_address"] = encode_puzzle_hash(test_ph, "txch")
+            # set pool to second private key
+            sk = wallet_node.get_key_for_fingerprint(pks[1])
+            test_ph = create_puzzlehash_for_pk(master_sk_to_wallet_sk(sk, uint32(0)).get_g1())
+            test_config["pool"]["xch_target_address"] = encode_puzzle_hash(test_ph, "txch")
+            save_config(wallet_node.root_path, "config.yaml", test_config)
+
+            # Check first key
+            sk_dict = await client.check_delete_key(pks[0])
+            assert sk_dict["fingerprint"] == pks[0]
+            assert sk_dict["used_for_farmer_rewards"] is True
+            assert sk_dict["used_for_pool_rewards"] is False
+
+            # Check second key
+            sk_dict = await client.check_delete_key(pks[1])
+            assert sk_dict["fingerprint"] == pks[1]
+            assert sk_dict["used_for_farmer_rewards"] is False
+            assert sk_dict["used_for_pool_rewards"] is True
+
+            # Check unknown key
+            sk_dict = await client.check_delete_key(123456)
+            assert sk_dict["fingerprint"] == 123456
+            assert sk_dict["used_for_farmer_rewards"] is False
+            assert sk_dict["used_for_pool_rewards"] is False
 
             await client.delete_key(pks[0])
             await client.log_in_and_skip(pks[1])
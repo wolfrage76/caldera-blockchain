Chia-Network/chia-blockchain

Pools mainnet (#7047)
* added clarifying comments

* WIP test

* added WIP test

* Refine genesis challenge. Remove unnecessary pool_puzzle function

* Sign spend. Remove create_member_spend. Rename state transition function to create_travel_spend

* Rename create_member_spend to create_travel_spend

* Add singleton id logging

* Enhance logging for debugging

* renaming

* rephrase inside the puzzle

* fixed signing and added some support functions

* Fix issue with announcement

* Progress spending the singleton

* Fix arguments to pool_state_to_inner_puzzle call

* Fix arguments to pool_state_to_inner_puzzle

* Improve error message when wallet is not running

* Remove misleading message about missing wallet process, when problem is the farmer by making poolnft command error out earlier

* Fix parent coin info bug

* Multiple state transitions in one block

* Lint

* Remove assert

* Fix incorrect p2_singleton_ph calculation (thanks nil00)

* Update waiting room puzzle to accept genesis_challenge

* Update calls to create_waiting

* Go to waiting state from committed state

* Augment debug_spend_bundle

* fix 2 bugs in wallet

* Fix lint

* fix bad_agg_sig bug

* Tests and lint

* remove breakpoint

* fix clvm tests for new hexs and hashs

* Fixed a bug in the coin store that was probably from merging. (#6577)

* Fixed a bug in the coin store that was probably from merging.

* The exception doesn't need to be there

* CI Lint fix

* Added lifecycle tests for pooling drivers (#6610)

* Ms.poolabsorb (#6615)

* Support for absorbing rewards in pools (untested)

* Style improvements

* More work on absorb

* Revert default root and remove log

* Revert small plots

* Use real sub slot iters

* Update types

* debug1

* Fix bugs

* fix output of agg sig log messages

* Make fewer calls to pw_status in test

* remove old comment

* logging and state management

* logg

chia/rpc/full_node_rpc_client.py
@@ -1,9 +1,12 @@
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple, Any
 
 from chia.consensus.block_record import BlockRecord
+from chia.full_node.signage_point import SignagePoint
 from chia.rpc.rpc_client import RpcClient
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.types.coin_record import CoinRecord
+from chia.types.coin_solution import CoinSolution
+from chia.types.end_of_slot_bundle import EndOfSubSlotBundle
 from chia.types.full_block import FullBlock
 from chia.types.spend_bundle import SpendBundle
 from chia.types.unfinished_header_block import UnfinishedHeaderBlock
@@ -72,6 +75,13 @@ class FullNodeRpcClient(RpcClient):
             return None
         return network_space_bytes_estimate["space"]
 
+    async def get_coin_record_by_name(self, coin_id: bytes32) -> Optional[CoinRecord]:
+        try:
+            response = await self.fetch("get_coin_record_by_name", {"name": coin_id.hex()})
+        except Exception:
+            return None
+        return CoinRecord.from_json_dict(response["coin_record"])
+
     async def get_coin_records_by_puzzle_hash(
         self,
         puzzle_hash: bytes32,
@@ -133,6 +143,13 @@ class FullNodeRpcClient(RpcClient):
     async def push_tx(self, spend_bundle: SpendBundle):
         return await self.fetch("push_tx", {"spend_bundle": spend_bundle.to_json_dict()})
 
+    async def get_puzzle_and_solution(self, coin_id: bytes32, height: uint32) -> Optional[CoinRecord]:
+        try:
+            response = await self.fetch("get_puzzle_and_solution", {"coin_id": coin_id.hex(), "height": height})
+            return CoinSolution.from_json_dict(response["coin_solution"])
+        except Exception:
+            return None
+
     async def get_all_mempool_tx_ids(self) -> List[bytes32]:
         response = await self.fetch("get_all_mempool_tx_ids", {})
         return [bytes32(hexstr_to_bytes(tx_id_hex)) for tx_id_hex in response["tx_ids"]]
@@ -148,5 +165,28 @@ class FullNodeRpcClient(RpcClient):
         try:
             response = await self.fetch("get_mempool_item_by_tx_id", {"tx_id": tx_id.hex()})
             return response["mempool_item"]
+        except Exception:
+            return None
+
+    async def get_recent_signage_point_or_eos(
+        self, sp_hash: Optional[bytes32], challenge_hash: Optional[bytes32]
+    ) -> Optional[Any]:
+        try:
+            if sp_hash is not None:
+                assert challenge_hash is None
+                response = await self.fetch("get_recent_signage_point_or_eos", {"sp_hash": sp_hash.hex()})
+                return {
+                    "signage_point": SignagePoint.from_json_dict(response["signage_point"]),
+                    "time_received": response["time_received"],
+                    "reverted": response["reverted"],
+                }
+            else:
+                assert challenge_hash is not None
+                response = await self.fetch("get_recent_signage_point_or_eos", {"challenge_hash": challenge_hash.hex()})
+                return {
+                    "eos": EndOfSubSlotBundle.from_json_dict(response["eos"]),
+                    "time_received": response["time_received"],
+                    "reverted": response["reverted"],
+                }
         except Exception:
             return None

tests/core/test_farmer_harvester_rpc.py
@@ -1,3 +1,5 @@
+# flake8: noqa: E501
+import logging
 from secrets import token_bytes
 
 import pytest
@@ -15,12 +17,15 @@ from chia.rpc.rpc_server import start_rpc_server
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.util.bech32m import decode_puzzle_hash, encode_puzzle_hash
 from tests.block_tools import get_plot_dir
-from chia.util.config import load_config
+from chia.util.byte_types import hexstr_to_bytes
+from chia.util.config import load_config, save_config
 from chia.util.hash import std_hash
 from chia.util.ints import uint8, uint16, uint32, uint64
-from chia.wallet.derive_keys import master_sk_to_wallet_sk
+from chia.wallet.derive_keys import master_sk_to_wallet_sk, master_sk_to_pooling_authentication_sk
 from tests.setup_nodes import bt, self_hostname, setup_farmer_harvester, test_constants
 from tests.time_out_assert import time_out_assert
+
+log = logging.getLogger(__name__)
 
 
 class TestRpc:
@@ -160,6 +165,10 @@ class TestRpc:
             res_2 = await client_2.get_plots()
             assert len(res_2["plots"]) == num_plots
 
+            # Test farmer get_plots
+            farmer_res = await client.get_plots()
+            assert len(list(farmer_res.values())[0]["plots"]) == num_plots
+
             assert len(await client_2.get_plot_directories()) == 1
 
             await client_2.add_plot_directory(str(plot_dir))
@@ -219,6 +228,35 @@ class TestRpc:
             replaced_char = new_ph_3_encoded[0:-1] + "a"
             with pytest.raises(ValueError):
                 await client.set_reward_targets(None, replaced_char)
+
+            assert len((await client.get_pool_state())["pool_state"]) == 0
+            all_sks = farmer_api.farmer.keychain.get_all_private_keys()
+            auth_sk = master_sk_to_pooling_authentication_sk(all_sks[0][0], 2, 1)
+            pool_list = [
+                {
+                    "launcher_id": "ae4ef3b9bfe68949691281a015a9c16630fc8f66d48c19ca548fb80768791afa",
+                    "authentication_public_key": bytes(auth_sk.get_g1()).hex(),
+                    "owner_public_key": "84c3fcf9d5581c1ddc702cb0f3b4a06043303b334dd993ab42b2c320ebfa98e5ce558448615b3f69638ba92cf7f43da5",
+                    "payout_instructions": "c2b08e41d766da4116e388357ed957d04ad754623a915f3fd65188a8746cf3e8",
+                    "pool_url": "localhost",
+                    "p2_singleton_puzzle_hash": "16e4bac26558d315cded63d4c5860e98deb447cc59146dd4de06ce7394b14f17",
+                    "target_puzzle_hash": "344587cf06a39db471d2cc027504e8688a0a67cce961253500c956c73603fd58",
+                }
+            ]
+            config["pool"]["pool_list"] = pool_list
+            save_config(root_path, "config.yaml", config)
+            await farmer_api.farmer.update_pool_state()
+
+            pool_state = (await client.get_pool_state())["pool_state"]
+            assert len(pool_state) == 1
+            assert (
+                pool_state[0]["pool_config"]["payout_instructions"]
+                == "c2b08e41d766da4116e388357ed957d04ad754623a915f3fd65188a8746cf3e8"
+            )
+            await client.set_payout_instructions(hexstr_to_bytes(pool_state[0]["pool_config"]["launcher_id"]), "1234vy")
+
+            pool_state = (await client.get_pool_state())["pool_state"]
+            assert pool_state[0]["pool_config"]["payout_instructions"] == "1234vy"
 
         finally:
             # Checks that the RPC manages to stop the node

tests/pools/test_pool_config.py
@@ -1,0 +1,43 @@
+# flake8: noqa: E501
+from pathlib import Path
+
+from blspy import AugSchemeMPL, PrivateKey
+
+from chia.pools.pool_config import PoolWalletConfig
+from chia.util.config import load_config, save_config, create_default_chia_config
+
+
+def test_pool_config():
+    test_root = Path("/tmp")
+    test_path = Path("/tmp/config")
+    eg_config = test_path / "config.yaml"
+    to_config = test_path / "test_pool_config.yaml"
+
+    create_default_chia_config(test_root, ["config.yaml"])
+    assert eg_config.exists()
+    eg_config.rename(to_config)
+    config = load_config(test_root, "test_pool_config.yaml")
+
+    auth_sk: PrivateKey = AugSchemeMPL.key_gen(b"1" * 32)
+    d = {
+        "authentication_public_key": bytes(auth_sk.get_g1()).hex(),
+        "owner_public_key": "84c3fcf9d5581c1ddc702cb0f3b4a06043303b334dd993ab42b2c320ebfa98e5ce558448615b3f69638ba92cf7f43da5",
+        "p2_singleton_puzzle_hash": "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824",
+        "payout_instructions": "c2b08e41d766da4116e388357ed957d04ad754623a915f3fd65188a8746cf3e8",
+        "pool_url": "localhost",
+        "launcher_id": "ae4ef3b9bfe68949691281a015a9c16630fc8f66d48c19ca548fb80768791afa",
+        "target_puzzle_hash": "344587cf06a39db471d2cc027504e8688a0a67cce961253500c956c73603fd58",
+    }
+
+    pwc = PoolWalletConfig.from_json_dict(d)
+
+    config_a = config.copy()
+    config_b = config.copy()
+
+    config_a["wallet"]["pool_list"] = [d]
+    config_b["wallet"]["pool_list"] = [pwc.to_json_dict()]
+
+    print(config["wallet"]["pool_list"])
+    save_config(test_root, "test_pool_config_a.yaml", config_a)
+    save_config(test_root, "test_pool_config_b.yaml", config_b)
+    assert config_a == config_b

wolfrage76/caldera-blockchain

caldera/rpc/full_node_rpc_client.py
@@ -1,9 +1,12 @@
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple, Any
 
 from caldera.consensus.block_record import BlockRecord
+from caldera.full_node.signage_point import SignagePoint
 from caldera.rpc.rpc_client import RpcClient
 from caldera.types.blockchain_format.sized_bytes import bytes32
 from caldera.types.coin_record import CoinRecord
+from caldera.types.coin_solution import CoinSolution
+from caldera.types.end_of_slot_bundle import EndOfSubSlotBundle
 from caldera.types.full_block import FullBlock
 from caldera.types.spend_bundle import SpendBundle
 from caldera.types.unfinished_header_block import UnfinishedHeaderBlock
@@ -72,6 +75,13 @@ class FullNodeRpcClient(RpcClient):
             return None
         return network_space_bytes_estimate["space"]
 
+    async def get_coin_record_by_name(self, coin_id: bytes32) -> Optional[CoinRecord]:
+        try:
+            response = await self.fetch("get_coin_record_by_name", {"name": coin_id.hex()})
+        except Exception:
+            return None
+        return CoinRecord.from_json_dict(response["coin_record"])
+
     async def get_coin_records_by_puzzle_hash(
         self,
         puzzle_hash: bytes32,
@@ -133,6 +143,13 @@ class FullNodeRpcClient(RpcClient):
     async def push_tx(self, spend_bundle: SpendBundle):
         return await self.fetch("push_tx", {"spend_bundle": spend_bundle.to_json_dict()})
 
+    async def get_puzzle_and_solution(self, coin_id: bytes32, height: uint32) -> Optional[CoinRecord]:
+        try:
+            response = await self.fetch("get_puzzle_and_solution", {"coin_id": coin_id.hex(), "height": height})
+            return CoinSolution.from_json_dict(response["coin_solution"])
+        except Exception:
+            return None
+
     async def get_all_mempool_tx_ids(self) -> List[bytes32]:
         response = await self.fetch("get_all_mempool_tx_ids", {})
         return [bytes32(hexstr_to_bytes(tx_id_hex)) for tx_id_hex in response["tx_ids"]]
@@ -148,5 +165,28 @@ class FullNodeRpcClient(RpcClient):
         try:
             response = await self.fetch("get_mempool_item_by_tx_id", {"tx_id": tx_id.hex()})
             return response["mempool_item"]
+        except Exception:
+            return None
+
+    async def get_recent_signage_point_or_eos(
+        self, sp_hash: Optional[bytes32], challenge_hash: Optional[bytes32]
+    ) -> Optional[Any]:
+        try:
+            if sp_hash is not None:
+                assert challenge_hash is None
+                response = await self.fetch("get_recent_signage_point_or_eos", {"sp_hash": sp_hash.hex()})
+                return {
+                    "signage_point": SignagePoint.from_json_dict(response["signage_point"]),
+                    "time_received": response["time_received"],
+                    "reverted": response["reverted"],
+                }
+            else:
+                assert challenge_hash is not None
+                response = await self.fetch("get_recent_signage_point_or_eos", {"challenge_hash": challenge_hash.hex()})
+                return {
+                    "eos": EndOfSubSlotBundle.from_json_dict(response["eos"]),
+                    "time_received": response["time_received"],
+                    "reverted": response["reverted"],
+                }
         except Exception:
             return None

tests/core/test_farmer_harvester_rpc.py
@@ -1,3 +1,5 @@
+# flake8: noqa: E501
+import logging
 from secrets import token_bytes
 
 import pytest
@@ -15,12 +17,15 @@ from caldera.rpc.rpc_server import start_rpc_server
 from caldera.types.blockchain_format.sized_bytes import bytes32
 from caldera.util.bech32m import decode_puzzle_hash, encode_puzzle_hash
 from tests.block_tools import get_plot_dir
-from caldera.util.config import load_config
+from caldera.util.byte_types import hexstr_to_bytes
+from caldera.util.config import load_config, save_config
 from caldera.util.hash import std_hash
 from caldera.util.ints import uint8, uint16, uint32, uint64
-from caldera.wallet.derive_keys import master_sk_to_wallet_sk
+from caldera.wallet.derive_keys import master_sk_to_wallet_sk, master_sk_to_pooling_authentication_sk
 from tests.setup_nodes import bt, self_hostname, setup_farmer_harvester, test_constants
 from tests.time_out_assert import time_out_assert
+
+log = logging.getLogger(__name__)
 
 
 class TestRpc:
@@ -160,6 +165,10 @@ class TestRpc:
             res_2 = await client_2.get_plots()
             assert len(res_2["plots"]) == num_plots
 
+            # Test farmer get_plots
+            farmer_res = await client.get_plots()
+            assert len(list(farmer_res.values())[0]["plots"]) == num_plots
+
             assert len(await client_2.get_plot_directories()) == 1
 
             await client_2.add_plot_directory(str(plot_dir))
@@ -219,6 +228,35 @@ class TestRpc:
             replaced_char = new_ph_3_encoded[0:-1] + "a"
             with pytest.raises(ValueError):
                 await client.set_reward_targets(None, replaced_char)
+
+            assert len((await client.get_pool_state())["pool_state"]) == 0
+            all_sks = farmer_api.farmer.keychain.get_all_private_keys()
+            auth_sk = master_sk_to_pooling_authentication_sk(all_sks[0][0], 2, 1)
+            pool_list = [
+                {
+                    "launcher_id": "ae4ef3b9bfe68949691281a015a9c16630fc8f66d48c19ca548fb80768791afa",
+                    "authentication_public_key": bytes(auth_sk.get_g1()).hex(),
+                    "owner_public_key": "84c3fcf9d5581c1ddc702cb0f3b4a06043303b334dd993ab42b2c320ebfa98e5ce558448615b3f69638ba92cf7f43da5",
+                    "payout_instructions": "c2b08e41d766da4116e388357ed957d04ad754623a915f3fd65188a8746cf3e8",
+                    "pool_url": "localhost",
+                    "p2_singleton_puzzle_hash": "16e4bac26558d315cded63d4c5860e98deb447cc59146dd4de06ce7394b14f17",
+                    "target_puzzle_hash": "344587cf06a39db471d2cc027504e8688a0a67cce961253500c956c73603fd58",
+                }
+            ]
+            config["pool"]["pool_list"] = pool_list
+            save_config(root_path, "config.yaml", config)
+            await farmer_api.farmer.update_pool_state()
+
+            pool_state = (await client.get_pool_state())["pool_state"]
+            assert len(pool_state) == 1
+            assert (
+                pool_state[0]["pool_config"]["payout_instructions"]
+                == "c2b08e41d766da4116e388357ed957d04ad754623a915f3fd65188a8746cf3e8"
+            )
+            await client.set_payout_instructions(hexstr_to_bytes(pool_state[0]["pool_config"]["launcher_id"]), "1234vy")
+
+            pool_state = (await client.get_pool_state())["pool_state"]
+            assert pool_state[0]["pool_config"]["payout_instructions"] == "1234vy"
 
         finally:
             # Checks that the RPC manages to stop the node

tests/pools/test_pool_config.py
@@ -1,0 +1,43 @@
+# flake8: noqa: E501
+from pathlib import Path
+
+from blspy import AugSchemeMPL, PrivateKey
+
+from caldera.pools.pool_config import PoolWalletConfig
+from caldera.util.config import load_config, save_config, create_default_caldera_config
+
+
+def test_pool_config():
+    test_root = Path("/tmp")
+    test_path = Path("/tmp/config")
+    eg_config = test_path / "config.yaml"
+    to_config = test_path / "test_pool_config.yaml"
+
+    create_default_caldera_config(test_root, ["config.yaml"])
+    assert eg_config.exists()
+    eg_config.rename(to_config)
+    config = load_config(test_root, "test_pool_config.yaml")
+
+    auth_sk: PrivateKey = AugSchemeMPL.key_gen(b"1" * 32)
+    d = {
+        "authentication_public_key": bytes(auth_sk.get_g1()).hex(),
+        "owner_public_key": "84c3fcf9d5581c1ddc702cb0f3b4a06043303b334dd993ab42b2c320ebfa98e5ce558448615b3f69638ba92cf7f43da5",
+        "p2_singleton_puzzle_hash": "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824",
+        "payout_instructions": "c2b08e41d766da4116e388357ed957d04ad754623a915f3fd65188a8746cf3e8",
+        "pool_url": "localhost",
+        "launcher_id": "ae4ef3b9bfe68949691281a015a9c16630fc8f66d48c19ca548fb80768791afa",
+        "target_puzzle_hash": "344587cf06a39db471d2cc027504e8688a0a67cce961253500c956c73603fd58",
+    }
+
+    pwc = PoolWalletConfig.from_json_dict(d)
+
+    config_a = config.copy()
+    config_b = config.copy()
+
+    config_a["wallet"]["pool_list"] = [d]
+    config_b["wallet"]["pool_list"] = [pwc.to_json_dict()]
+
+    print(config["wallet"]["pool_list"])
+    save_config(test_root, "test_pool_config_a.yaml", config_a)
+    save_config(test_root, "test_pool_config_b.yaml", config_b)
+    assert config_a == config_b